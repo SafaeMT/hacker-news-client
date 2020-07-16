c70a5a003d01ee3e3f8bdb6c87f086c582cf2dc6
Safae Mliha-Touati <57368349+SafaeMT@users.noreply.github.com>
2020-07-16 22:56:14 +0200

    display the search result

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const API_URL = "https://hn.algolia.com/api/v1/search";
 const searchInput = document.querySelector(".input");
+const thead = document.querySelector("thead");
+const tbody = document.querySelector("tbody");
 
 document.querySelector(".button").addEventListener("click", handleRequest);
 
@@ -19,4 +21,28 @@ function handleResponse({ hits }) {
     num_comments: hit.num_comments,
     points: hit.points,
   }));
+
+  renderStories(stories);
+}
+
+function renderStories(stories) {
+  thead.innerHTML = `<tr class="has-background-primary">
+    <th class="has-text-white">TITLE</th>
+    <th class="has-text-white">AUTHOR</th>
+    <th class="has-text-white">COMMENTS</th>
+    <th class="has-text-white">POINTS</th>
+  </tr>`;
+
+  let result = "";
+
+  stories.forEach((story) => {
+    result += `<tr>
+      <td class="has-text-left"><a href="${story.url}">${story.title}</a></td>
+      <td>${story.author}</td>
+      <td>${story.num_comments}</td>
+      <td>${story.points}</td>
+    </tr>`;
+  });
+
+  tbody.innerHTML = result;
 }
