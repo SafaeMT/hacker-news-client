42110524f19dfe1c00affaab5a363c1eb7b2f8af
Safae Mliha-Touati <57368349+SafaeMT@users.noreply.github.com>
2020-07-16 22:02:24 +0200

    select + store data we want to display for the first 20 stories

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ document.querySelector(".button").addEventListener("click", handleRequest);
 function handleRequest() {
   const query = searchInput.value;
 
-  fetch(`${API_URL}?query=${query}&tags=story`).then((response) =>
-    response.json()
-  );
+  fetch(`${API_URL}?query=${query}&tags=story`)
+    .then((response) => response.json())
+    .then((jsonResponse) => handleResponse(jsonResponse));
+}
+
+function handleResponse({ hits }) {
+  const stories = hits.map((hit) => ({
+    url: hit.url,
+    author: hit.author,
+    title: hit.title,
+    num_comments: hit.num_comments,
+    points: hit.points,
+  }));
 }
